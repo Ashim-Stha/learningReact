aa24b8a590b31f012e92218ac28034611ca8a716
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-06-24 14:38:11 +0545

    ChangeColorModified

--- src/ChangeColor.js
+++ src/ChangeColor.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import colorNames from "colornames";
 
 const ChangeColor = () => {
+  // const black = { black };
+  // console.log(black)
+  // localStorage.setItem("hexvalue", black);
+
   const [color, setColor] = useState(JSON.parse(localStorage.getItem("color")));
+  const [hexvalue, setHexvalue] = useState(
+    JSON.parse(localStorage.getItem("hexvalue"))
+  );
+  const [isDarktext, setIsDarktext] = useState(true);
   const handleSubmit = (e) => {
     e.preventDefault();
     localStorage.setItem("color", JSON.stringify(color));
+    localStorage.setItem("hexvalue", JSON.stringify(hexvalue));
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -14,17 +24,27 @@ const ChangeColor = () => {
           width: "200px",
           border: "2px solid black",
           margin: "auto",
-          backgroundColor: `${color}`,
+          backgroundColor: color,
+          color: isDarktext ? "black" : "white",
         }}
       >
-        {color}
+        <p>{color ? color : "Empty"}</p>
+        <p>{hexvalue ? hexvalue : "null"}</p>
       </div>
       <input
+        autoFocus
         type="text"
         placeholder="Change Color"
+        required
         value={color}
-        onChange={(e) => setColor(e.target.value)}
+        onChange={(e) => {
+          setColor(e.target.value);
+          setHexvalue(colorNames(e.target.value));
+        }}
       />
+      <button type="button" onClick={() => setIsDarktext(!isDarktext)}>
+        Toggle Textcolor
+      </button>
     </form>
   );
 };
