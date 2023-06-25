3e1bacda3f6bd76147ba559a5e9155bd555a22d6
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-06-25 21:56:42 +0545

    myway

--- src/ListforFetchApi.js
+++ src/ListforFetchApi.js
@@ -5,8 +5,35 @@ const ListforFetchApi = ({ items }) => {
     <ul>
       {items.map((item) => (
         <li key={item.id}>
-          {JSON.stringify(item)}
-          {/* <p>{item.name}</p> */}
+          {/* {JSON.stringify(item)} */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              border: "1px solid black",
+            }}
+          >
+            <p
+              style={{
+                border: "1px solid black",
+              }}
+            >
+              {item.id}
+            </p>
+            <p
+              style={{
+                border: "1px solid black",
+              }}
+            >
+              {item.name}
+            </p>
+            <p>{item.username}</p>
+            <p>{item.email}</p>
+            <p>{JSON.stringify(item.address)}</p>
+            <p>{item.phone}</p>
+            <p>{item.website}</p>
+            <p>{JSON.stringify(item.company)}</p>
+          </div>
         </li>
       ))}
     </ul>
