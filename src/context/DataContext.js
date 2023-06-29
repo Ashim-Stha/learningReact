0b6aad44b616a6c32620454e289bf8eaf349bd12
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-06-29 22:02:09 +0545

    useContext-I

---
+++ src/context/DataContext.js
@@ -0,0 +1,9 @@
+import React, { useEffect, createContext, useState } from "react";
+
+const DataContext = createContext({});
+
+export const DataProvider = ({ children }) => {
+  return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+};
+
+export default DataContext;
